72ecbeb54b2129d43500b8a7eb69766969f500a1
Alan <alan.join@laposte.net>
2025-08-15 00:54:27 +0200

    Ajout de commentaires + rename des variables

--- script.js
+++ script.js
@@ -1,96 +1,106 @@
-// On met par défaut la date d'aujourd'hui dans le selecteur d'alarme
-document.getElementById("alarmDate").valueAsDate = new Date();
+/**
+ * Script permettant d'afficher l'heure sur le site, ainsi que de définir les alarmes et créer les alertes
+ * Auteur : Alan Join
+ * 
+ */
 
+/* Configuration des éléments par défauts sur le site*/
+document.getElementById("dateAlarme").valueAsDate = new Date(); // On met par défaut la date d'aujourd'hui dans le selecteur d'alarme
 
-let time = document.getElementById("time");
-let dateInput = document.getElementById("alarmDate");
-let tInput = document.getElementById("alarmTime");
+/* Variables du script */
+let temps = document.getElementById("temps"); // Horloge à afficher
+
+// Paramètres de l'alarme
 let btn = document.getElementById("setAlarm");
-let contan = document.getElementById("alarms");
-let interVal;
-let almTimesArray = [];
 let musique_alarme = document.getElementById("musique_alarme");
 
-function timeChangeFunction() {
-	let curr = new Date();
-	let hrs = String(curr.getUTCHours()).padStart(2, "0");
-	let min = String(curr.getUTCMinutes()).padStart(2, "0");
-	let sec = String(curr.getUTCSeconds()).padStart(2, "0");
-	time.textContent = `Temps actuel \n ${hrs}:${min}:${sec} UTC`;
+// Date renseignée pour définir l'alarme
+let dateInput = document.getElementById("dateAlarme");
+let tInput = document.getElementById("alarmTime");
+
+// Variables pour afficher les alarmes déjà présentes
+let histoAlarme = document.getElementById("alarms");
+let listeTempsAlarme = [];
+
+let interVal; // Interval entre l'alarme et l'heure actuelle
+
+/** 
+ * Fonction permettant d'afficher l'heure en temps réel sur le HTML
+*/
+function afficherHeure() {
+	let now = new Date();
+	let hrs = String(now.getUTCHours()).padStart(2, "0");
+	let min = String(now.getUTCMinutes()).padStart(2, "0");
+	let sec = String(now.getUTCSeconds()).padStart(2, "0");
+	temps.textContent = `Temps actuel \n ${hrs}:${min}:${sec} UTC`;
 }
 
-
-function alarmSetFunction() {
+/**
+ * Fonction permettant d'ajouter une alarme ainsi que de la déclencher si elle est atteinte
+ */
+function gestionAlarme() {
 	let now = new Date();
-	let selectedDate = new Date(dateInput.value + "T" + tInput.value+"Z");
-	if (selectedDate.valueOf() <= now.valueOf()) {
+	let dateSelectionnee = new Date(dateInput.value + "T" + tInput.value+"Z");
+    let tempsAvantAlarme = dateSelectionnee - now;
+
+    // Vérifie si l'alarme à ajouter n'est pas antérieure ou déjà établie
+	if (dateSelectionnee.valueOf() <= now.valueOf()) {
 		alert(`Date non valide : veuillez renseigner une date future`);
 		return;
 	}
-	if (almTimesArray.includes(selectedDate.toString())) {
+	if (listeTempsAlarme.includes(dateSelectionnee.toString())) {
 		alert(`Alarme déjà établie`);
 		return;
 	}
 
-    let timeUntilAlarm = selectedDate - now;
-    let alarmDiv = document.createElement("div");
-    alarmDiv.classList.add("alarm");
-    alarmDiv.innerHTML = `
+    //Ajout de l'alarme sur le HTML
+    let alarmeDiv = document.createElement("div");
+    alarmeDiv.classList.add("alarm");
+    alarmeDiv.innerHTML = `
         <span>
-        ${selectedDate.toUTCString()}
+        ${dateSelectionnee.toUTCString()}
         </span>
         <button class="delete-alarm">
         Delete
         </button>
     `;
-    alarmDiv
+    alarmeDiv
         .querySelector(".delete-alarm")
         .addEventListener("click", () => {
-            alarmDiv.remove();
+            alarmeDiv.remove();
             clearTimeout(interVal);
-            const idx = almTimesArray.indexOf(selectedDate.toString());
+            const idx = listeTempsAlarme.indexOf(dateSelectionnee.toString());
             if (idx !== -1) {
-                almTimesArray.splice(idx, 1);
+                listeTempsAlarme.splice(idx, 1);
             }
         });
+
+    // Met en place le compte à rebours et la popUp à afficher
     interVal = setTimeout(() => {
         musique_alarme.play();
         alert("Il est l'heure d'envoyer les TAFs !");
         musique_alarme.pause();
         musique_alarme.currentTime = 0;
-        alarmDiv.remove();
-        const alarmIndex = almTimesArray.indexOf(selectedDate.toString());
+        alarmeDiv.remove();
+        const alarmIndex = listeTempsAlarme.indexOf(dateSelectionnee.toString());
         if (alarmIndex !== -1) {
-            almTimesArray.splice(alarmIndex, 1);
+            listeTempsAlarme.splice(alarmIndex, 1);
         }
-    }, timeUntilAlarm);
-    contan.appendChild(alarmDiv);
-    almTimesArray.push(selectedDate.toString());
+    }, tempsAvantAlarme);
+    histoAlarme.appendChild(alarmeDiv);
+    listeTempsAlarme.push(dateSelectionnee.toString());
 }
 
-function showAlarmFunction() {
-	let alarms = contan.querySelectorAll(".alarm");
-	alarms.forEach((alarm) => {
-		let deleteButton = alarm.querySelector(".delete-alarm");
-
-		deleteButton.addEventListener("click", () => {
-			alarmDiv.remove();
-			clearTimeout(interVal);
-			const alarmIndex = almTimesArray.indexOf(selectedDate.toString());
-			if (alarmIndex !== -1) {
-				almTimesArray.splice(alarmIndex, 1);
-			}
-		});
-
-	});
-}
-
+/**
+ * Activation du mode Madonna (pour plus de plaisir)
+ */
 function madonnaMode() {
-  var element = document.body;
+  var bodyDuHTML = document.body;
   var checkMadonna = document.getElementById("checkMadonna");
-  element.classList.toggle("madonna-mode");
 
+  bodyDuHTML.classList.toggle("madonna-mode"); // Changement de style
 
+  // Changement de musique
   if (checkMadonna.checked == true) {
 	musique_alarme = document.getElementById("musique_alarme_madonna");
   }
@@ -99,9 +109,7 @@ function madonnaMode() {
   }
 } 
 
-
-showAlarmFunction();
-setInterval(timeChangeFunction, 1000);
-btn.addEventListener("click", alarmSetFunction);
-timeChangeFunction();
-// Penser à trier la lise des dates à chaque input
+afficherHeure();
+setInterval(afficherHeure, 1000);
+btn.addEventListener("click", gestionAlarme);
+// Penser à trier la liste des dates à chaque input
